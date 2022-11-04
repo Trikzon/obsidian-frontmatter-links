afe0e7b2eff610a33483c09c95c0b2356f32367b
Michael Naumov <mnaoumov@gmail.com>
2022-11-04 16:51:04 -0600

    Add support for links without quotes

--- src/editor_plugin.ts
+++ src/editor_plugin.ts
@@ -19,57 +19,104 @@ export class FrontmatterLinksEditorPlugin implements PluginValue {
 
     destroy() { }
 
+    processLink(view: EditorView, builder: RangeSetBuilder<Decoration>, from: number, to: number, includesQuotes: boolean) {
+        const linkFrom = includesQuotes ? from + 1 : from;
+        const linkTo = includesQuotes ? to - 1 : to;
+        const text = view.state.sliceDoc(linkFrom, linkTo);
+
+        let href = null;
+        let alias = null;
+        if (text.substring(0, 2) === "[[" && text.substring(text.length - 2) === "]]") {
+            href = text.substring(2, text.length - 2);
+            const aliasIndex = href.indexOf("|");
+            if (aliasIndex !== -1) {
+                alias = href.substring(aliasIndex + 1);
+                href = href.substring(0, aliasIndex);
+            }
+        } else if (text[0] === "[" && text[text.length - 1] === ")") {
+            const aliasClose = text.indexOf("]");
+            const hrefOpen = text.indexOf("(");
+            if (aliasClose !== -1 && hrefOpen !== -1) {
+                alias = text.substring(1, aliasClose);
+                href = text.substring(hrefOpen + 1, text.length - 1);
+            }
+        } else if (isUri(text)) {
+            href = text;
+        }
+
+        if (href) {
+            alias = !alias ? href : alias;
+            const cursorHead = view.state.selection.main.head;
+            if (from - 1 <= cursorHead && cursorHead <= to + 1) {
+                // TODO: When the cursor is next to or on the link, style it like Obsidian does.
+                builder.add(
+                    linkFrom,
+                    linkTo,
+                    Decoration.mark({
+                        class: "cm-url"
+                    })
+                )
+            } else {
+                builder.add(
+                    from,
+                    to,
+                    Decoration.replace({
+                        widget: new FrontmatterLinkWidget(href, alias, isUri(href) === undefined)
+                    })
+                )
+            }
+        }
+    }
+
     buildDecorations(view: EditorView): DecorationSet {
         const builder = new RangeSetBuilder<Decoration>();
+        const plugin = this;
+
+        let linkFrom: number | null;
+        let linkTo: number;
 
         for (let { from, to } of view.visibleRanges) {
             syntaxTree(view.state).iterate({
                 from,
                 to,
                 enter(node) {
-                    if (node.name === "hmd-frontmatter_string") {
-                        const text = view.state.sliceDoc(node.from + 1, node.to - 1);
-
-                        let href = null;
-                        let alias = null;
-                        if (text.substring(0, 2) === "[[" && text.substring(text.length - 2) === "]]") {
-                            href = text.substring(2, text.length - 2);
-                            const aliasIndex = href.indexOf("|");
-                            if (aliasIndex !== -1) {
-                                alias = href.substring(aliasIndex + 1);
-                                href = href.substring(0, aliasIndex);
-                            }
-                        } else if (text[0] === "[" && text[text.length - 1] === ")") {
-                            const aliasClose = text.indexOf("]");
-                            const hrefOpen = text.indexOf("(");
-                            if (aliasClose !== -1 && hrefOpen !== -1) {
-                                alias = text.substring(1, aliasClose);
-                                href = text.substring(hrefOpen + 1, text.length - 1);
-                            }
-                        } else if (isUri(text)) {
-                            href = text;
-                        }
-
-                        if (href) {
-                            alias = !alias ? href : alias;
-                            const cursorHead = view.state.selection.main.head;
-                            if (node.from - 1 <= cursorHead && cursorHead <= node.to + 1) {
-                                // TODO: When the cursor is next to or on the link, style it like Obsidian does.
-                                builder.add(
-                                    node.from + 1,
-                                    node.to - 1,
-                                    Decoration.mark({
-                                        class: "cm-url"
-                                    })
-                                )
+                    if (linkFrom) {
+                        if (node.name === "hmd-frontmatter") {
+                            const text = view.state.sliceDoc(node.from, node.to);
+                            const spaceIndex = text.indexOf(" ");
+                            if (spaceIndex !== -1) {
+                                linkTo = node.from + spaceIndex;
+                                plugin.processLink(view, builder, linkFrom, linkTo, false);
+                                linkFrom = null;
                             } else {
-                                builder.add(
-                                    node.from,
-                                    node.to,
-                                    Decoration.replace({
-                                        widget: new FrontmatterLinkWidget(href, alias, isUri(href) === undefined)
-                                    })
-                                )
+                                linkTo = node.to;
+                            }
+                        } else if (node.name == "hmd-frontmatter_meta") {
+                            linkTo = node.to;
+                        } else {
+                            plugin.processLink(view, builder, linkFrom, linkTo, false);
+                            linkFrom = null;
+                        }
+                    }
+
+                    if (!linkFrom) {
+                        if (node.name === "hmd-frontmatter_string") {
+                            plugin.processLink(view, builder, node.from, node.to, true);
+                        }
+                        
+                        if (node.name == "hmd-frontmatter") {
+                            const text = view.state.sliceDoc(node.from, node.to);
+
+                            for (const urlPrefix of ["http", "https", "file"]) {
+                                if (text == urlPrefix) {
+                                    linkFrom = node.from;
+                                    linkTo = node.to;
+                                    break;
+                                } else if (text.endsWith(" " + urlPrefix)) {
+                                    linkFrom = node.to - urlPrefix.length;
+                                    linkTo = node.to;
+                                    break;
+                                }
                             }
                         }
                     }
